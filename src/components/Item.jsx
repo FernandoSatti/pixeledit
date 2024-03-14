70b510e012632538c9c98edead43d7b45596640e
Fernando Ismael Satti <125413132+FernandoSatti@users.noreply.github.com>
2024-03-14 20:08:01 -0300

    Update Item.jsx
    se realizo un cambio para colocar un efecto para hacer mas intuitivo la selección de cards.

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,27 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-export const Item =  ({ product }) => {
+export const Item = ({ product }) => {
+  const [showDescription, setShowDescription] = useState(false);
+
+  useEffect(() => {
+    setShowDescription(true);
+
+    const timer = setTimeout(() => {
+      setShowDescription(false);
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-      <div className="card-container">
-        <div className="card-container-product">
-          <Link to={product.url}>
-            <div className="card-product">
-              <div className="front-content-product">
-                <p>{product.title}</p>
-              </div>
-              <div className="content-product">
-                <p className="heading-product">{product.title}</p>
-                <p>{product.description}</p>
-              </div>
+    <div className="card-container">
+      <div className="card-container-product">
+        <Link to={product.url}>
+          <div className="card-product">
+            <div className="front-content-product">
+              <p>{product.title}</p>
             </div>
-            </Link>
-          
-        </div>
-        <Link to={`/product/${product.id}`}>
-        <button className="button-service button-pack">
-            {product.button}
-          </button>
+            <div
+              className={`content-product ${
+                showDescription ? "show-description" : ""
+              }`}
+            >
+              <p className="heading-product">{product.title}</p>
+              <p>{product.description}</p>
+            </div>
+          </div>
         </Link>
       </div>
+      <Link to={`/product/${product.id}`}>
+        <button className="button-service button-pack">
+          {product.button}
+        </button>
+      </Link>
+    </div>
   );
-}
+};
